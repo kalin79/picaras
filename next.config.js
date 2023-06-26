02a87c91ef928af90a153929a45af49ec933c8dd
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2023-06-26 08:58:38 -0500

    Innicialinzando configuracion basica

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
 }
 
 module.exports = nextConfig
+
